ef17649554b9f01aea4258d8372278fb9556b86c
Arun Mummidi <arunmummidi@gmail.com>
2023-08-18 00:58:48 +0530

    beautify

--- projects/flash_single_led.py
+++ projects/flash_single_led.py
@@ -1,30 +1,33 @@
 # Simple single LED control
 # program to familiarize with GPIO
 
-# Import Raspberry GPIO library
-import RPi.GPIO as GPIO
+"""Import Raspberry GPIO library"""
 import time
+from RPi import GPIO
 
 # Set GPIO Pins as Broadcom interfaced
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Define the GPIO control Pin
-led_control_pin = 17
+CONTROL_PIN = 17
 
 # Set the GPIO pin as Output
-GPIO.setup(led_control_pin,GPIO.OUT)
+GPIO.setup(CONTROL_PIN,GPIO.OUT)
 
 print("LED control pin is set to:")
-print(led_control_pin)
+print(CONTROL_PIN)
 
-while True:
+for count in range(0, 10):
     # Set GPIO PIN to High
-    GPIO.output(led_control_pin, GPIO.HIGH)
+    GPIO.output(CONTROL_PIN, GPIO.HIGH)
     print ("LED is ON")
-    time.sleep(5)
+    time.sleep(1)
     
     #Set GPIO PIN to Low
-    GPIO.output(led_control_pin, GPIO.LOW)
+    GPIO.output(CONTROL_PIN, GPIO.LOW)
     print ("LED is OFF")
-    time.sleep(5)
+    time.sleep(1)
+
+GPIO.cleanup()
+print("Clean up done")
