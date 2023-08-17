70fed89bc31cf8247b292db52f5b990f5754cccb
Arun Mummidi <arunmummidi@gmail.com>
2023-08-17 12:56:00 +0530

    document

--- projects/single_relay_control.py
+++ projects/single_relay_control.py
@@ -1,4 +1,16 @@
-# Simple single relay module control
+# WARNING: If you plan to use relay
+# with AC ouptputs, use extreme 
+# caution as it could potentialy
+# cause injury or death.
+
+# Simple single relay module control.
+# 1.5V to 3V LED/DC motor is connected
+# to relay output to test the relay 
+# operation. This will prevent injury
+# or component breakdown due to improper
+# AC connections. This project helps to
+# familiarize  with the circuit only.
+
 
 # Import Raspberry GPIO library
 import RPi.GPIO as GPIO
@@ -14,15 +26,15 @@ relay_control_pin = 17
 # Set the GPIO pin as relay 1 control
 GPIO.setup(relay_control_pin,GPIO.OUT)
 
-for count in range(0, 20):
+for count in range(0, 5):
     # Set GPIO PIN to High
     GPIO.output(relay_control_pin, GPIO.HIGH)
-    print ("Relay is ON. Wait 5 seconds")
+    print ("Relay is ON. Keep state for 5 seconds")
     time.sleep(5)
 
     #Set GPIO PIN to Low
     GPIO.output(relay_control_pin, GPIO.LOW)
-    print ("Relay is OFF. Wait 5 seconds")
+    print ("Relay is OFF. Keep state for 5 seconds")
     time.sleep(5)
 
 GPIO.cleanup()
